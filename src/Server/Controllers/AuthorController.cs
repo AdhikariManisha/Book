9663a6fe28f3ea9706b43e2a0b3e7ce45362000d
Manisha Adhikari <manisha.adhikari@dominiondms.com>
2022-11-23 09:25:05 -0500

    added crud operation

--- src/Server/Controllers/AuthorController.cs
+++ src/Server/Controllers/AuthorController.cs
@@ -1,4 +1,5 @@
 ﻿using Book.Application.Contracts.Repositories;
+using Book.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Book.Server.Controllers;
@@ -16,13 +17,41 @@ public class AuthorController : ControllerBase
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult> GetAsync(int id) { 
+    public async Task<ActionResult> GetAsync(int id)
+    {
         var author = await _authorRepository.GetAsync(id);
         return Ok(author);
     }
+
+    /// <summary>
+    ///  Get all authors
+    /// </summary>
+    /// <returns>Status 200 ok</returns>
     [HttpGet]
-    public async Task<ActionResult> GetListAsync() {
+    public async Task<ActionResult> GetListAsync()
+    {
         var authors = await _authorRepository.GetListAsync();
         return Ok(authors);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateAsync(Author input)
+    {
+        var taskCompleted = await _authorRepository.CreateAsync(input);
+        return Ok(taskCompleted);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAsync(int id)
+    {
+        var taskCompleted = await _authorRepository.DeleteAsync(id);
+        return Ok(taskCompleted);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAsync(int id, Author input)
+    {
+        var taskCompleted = await _authorRepository.UpdateAsync(id, input);
+        return Ok(taskCompleted);
     }
 }
