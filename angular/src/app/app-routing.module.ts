93f80c29584c99c25154f89f7cd60d1a58d92140
AdhikariManisha <urmanisha.ad@gmail.com>
2023-08-02 10:59:33 -0400

    Added homepage compontent

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
   {path:'books', component: BooksComponent, canActivate: [AuthGuardService]},
   {path:'authors', component: AuthorsComponent, canActivate: [AuthGuardService]},
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule), canActivate: [AuthGuardService] },
 ];
 
 @NgModule({
